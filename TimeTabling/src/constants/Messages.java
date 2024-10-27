4c1dedea39ed279d6f0cc0bc7d58bf4d72d767f2
Francoo86 <115720207+Francoo86@users.noreply.github.com>
2024-10-27 20:45:42 -0300

    add message class (TODO: Enum)

---
+++ TimeTabling/src/constants/Messages.java
@@ -0,0 +1,7 @@
+package constants;
+
+public class Messages {
+    public static final String START = "START";
+    public static final String STOP = "STOP";
+    public static final String CONFIRM = "CONFIRM";
+}
