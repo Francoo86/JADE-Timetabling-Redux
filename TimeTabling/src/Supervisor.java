406635e8f6132f123385356ffa798113582cdc84
Francoo86 <115720207+Francoo86@users.noreply.github.com>
2024-10-27 17:54:06 -0300

    add supervisor agent

---
+++ TimeTabling/src/Supervisor.java
@@ -0,0 +1,8 @@
+import jade.core.Agent;
+
+public class Supervisor extends Agent {
+    @Override
+    protected void setup() {
+        System.out.println("Hola, soy el agente supervisor");
+    }
+}
