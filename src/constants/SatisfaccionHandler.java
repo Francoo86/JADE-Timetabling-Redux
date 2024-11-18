d739cc2b569118d3e83885154e890bee585b5677
Francoo86 <115720207+Francoo86@users.noreply.github.com>
2024-11-17 21:04:20 -0300

    make private enum for satisfaccion

--- src/constants/SatisfaccionHandler.java
+++ src/constants/SatisfaccionHandler.java
@@ -7,4 +7,18 @@ public class SatisfaccionHandler {
 
         return Satisfaccion.BAJA.getValor();
     }
+
+    private enum Satisfaccion {
+        ALTA(10),
+        MEDIA(5),
+        BAJA(3);
+
+        private final int valor;
+        Satisfaccion(int valor) {
+            this.valor = valor;
+        }
+        public int getValor() {
+            return valor;
+        }
+    }
 }
