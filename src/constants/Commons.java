4c1578d99de5c623d34eb1241262f144df69f6ef
Francoo86 <115720207+Francoo86@users.noreply.github.com>
2024-11-26 20:06:15 -0300

    add commons constant file

---
+++ src/constants/Commons.java
@@ -0,0 +1,6 @@
+package constants;
+
+public class Commons {
+    public static final String [] DAYS = {"Lunes", "Martes", "Miércoles", "Jueves", "Viernes", "Sábado", "Domingo"};
+    public static final int MAX_BLOQUE_DIURNO = 9;
+}
